6a29ec34cef2981fa9ae9a27bc5e67b891b23bac
Сергей <boikoff@inbox.ru>
2020-08-07 17:10:55 +0400

    Initial commit

---
+++ SergeiBoikov-SSIS/Scripts/1.bat
@@ -0,0 +1,2 @@
+C:\Python3\python.exe C:\GIT\Quantori\SergeiBoikov-SSIS\SergeiBoikov-SSIS\Scripts\TransformCsv.py C:\GIT\Quantori\SergeiBoikov-SSIS\SergeiBoikov-SSIS\SourceFiles\CoordStatusMA_Short.csv
+stop
